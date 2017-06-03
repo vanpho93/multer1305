f458ee1160d586a4322c1676ebf394359dc2d288
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-03 08:32:56 +0700

    upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const multer = require('multer');//1
 
+const upload = multer({ dest: './public' });//2
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -7,5 +9,10 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
+
+app.post('/upload', upload.single('hinhDaiDien'),(req, res) => {//3
+    console.log(req.body, req.file);
+    res.send('UPLOAD THANH CONG')
+});
 
 app.listen(3000, () => console.log('Server started!'));

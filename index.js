f0adb87dde03fad9cc62e74d203d31b83d763298
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-03 09:46:25 +0700

    xong

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
 
 app.post('/upload', upload.single('hinhDaiDien'),(req, res) => {//3
-    console.log(req.body, req.file);
+    console.log(req.body, req.file.filename);
+    res.send('UPLOAD THANH CONG')
+});
+
+app.post('/uploadArray', upload.fields([
+        { name: 'hinhChinh', maxCount: 1 },
+        { name: 'hinhPhu', maxCount: 2 }
+    ]), (req, res) => {
+    console.log(req.body, req.files);
     res.send('UPLOAD THANH CONG')
 });
 
